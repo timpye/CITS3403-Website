a7c86795c0a39663fb24968dfe5e864615ade19e
timpye <72972212+timpye@users.noreply.github.com>
2021-05-03 16:13:02 +0800

    Added routes to pages in website

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,23 @@ from flask import render_template, flash, redirect
 @app.route('/index')
 def index():
     return render_template('index.html', title = 'Home')
+
+@app.route('/quiz')
+def quiz():
+    return render_template('quiz.html', title = 'Quiz')
+
+@app.route('/stats')
+def stats():
+    return render_template('stats.html', title = 'Statistics')
+
+@app.route('/content.html')
+def content():
+    return render_template('content.html', title = 'Content')
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html', title = 'Feedback')
+
+@app.route('/login'):
+def login():
+    return render_template('login.html', title = 'Login')
