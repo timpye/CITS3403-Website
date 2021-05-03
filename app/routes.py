ce9716bcf83f3e9c1904fadb81f10e520e008c99
anferneealviar99 <anferneealviar99@hotmail.com>
2021-05-03 19:09:49 +0800

    Login form stuff

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,21 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
 
 @app.route('/')
+
 @app.route('/index')
 def index():
     return render_template('index.html', title = 'Home')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
+
+    return render_template('login.html', title='Login', form=form)
 
 @app.route('/quiz')
 def quiz():
@@ -24,8 +33,3 @@ def content():
 def feedback():
     return render_template('feedback.html', title = 'Feedback')
 
-@app.route('/login')
-def login():
-    form = LoginForm()
-
-    return render_template('login.html', title = 'Login')
