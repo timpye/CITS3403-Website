370ababf21175409c5400a341d98bc417deb5496
anferneealviar99 <anferneealviar99@hotmail.com>
2021-05-03 18:33:30 +0800

    import LoginForm from app.forms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template, flash, redirect
+from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
 
 @app.route('/')
