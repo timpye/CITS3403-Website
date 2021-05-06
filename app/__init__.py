548c2a193dadb0fb5720d9bcc1a1825d570ff1e6
jakobdorian <79760777+jakobdorian@users.noreply.github.com>
2021-05-06 13:42:59 +0800

    added static folder for images

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.config.from_object(Config)
 login = LoginManager(app)
 login.login_view = 'login'
